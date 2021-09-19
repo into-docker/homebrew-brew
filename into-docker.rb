c4c7c211d52ca9e1ea98b931944bfb757360c0ef
xsc <xsc@users.noreply.github.com>
2021-09-19 10:22:47 +0000

    Formula: into-docker@1.1.2

--- into-docker.rb
+++ into-docker.rb
@@ -1,14 +1,14 @@
 class IntoDocker < Formula
   desc "Never write another Dockerfile"
   homepage "https://github.com/into-docker/into-docker"
-  version "1.1.1"
+  version "1.1.2"
 
   if OS.linux?
-    url "https://github.com/into-docker/into-docker/releases/download/v1.1.1/into-1.1.1-linux-amd64.zip"
-    sha256 "ca005d0da31a07b71be3c7405a58cddb6c8868121587b7c96474391b3dbcf588"
+    url "https://github.com/into-docker/into-docker/releases/download/v1.1.2/into-1.1.2-linux-amd64.zip"
+    sha256 "1975a9a92bec020a2cc1868b0ff1df8fb8fb2523648881d9d0f26d5d8d40ea82"
   else
-    url "https://github.com/into-docker/into-docker/releases/download/v1.1.1/into-1.1.1-macos-amd64.zip"
-    sha256 "5ed65ff1314d17e20ee201d7563c8a6d98a209e6a654a21d5af5ddcc2e58fcd4"
+    url "https://github.com/into-docker/into-docker/releases/download/v1.1.2/into-1.1.2-macos-amd64.zip"
+    sha256 "9082ca994906b869012d8da469e1a3ddc4ff83aeab219e5110eeb5596ccf4c65"
   end
 
   bottle :unneeded
