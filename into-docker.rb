29d056563765a5f7bdb152309d728aa473fe046e
xsc <xsc@users.noreply.github.com>
2022-03-18 14:11:42 +0000

    Formula: into-docker@1.1.6

--- into-docker.rb
+++ into-docker.rb
@@ -1,14 +1,14 @@
 class IntoDocker < Formula
   desc "Never write another Dockerfile"
   homepage "https://github.com/into-docker/into-docker"
-  version "1.1.5"
+  version "1.1.6"
 
   if OS.linux?
-    url "https://github.com/into-docker/into-docker/releases/download/v1.1.5/into-1.1.5-linux-amd64.zip"
-    sha256 "a6e0f589874c162ac5580829c8d4e194790dac9911229888ad2af9dd772db0b6"
+    url "https://github.com/into-docker/into-docker/releases/download/v1.1.6/into-1.1.6-linux-amd64.zip"
+    sha256 "22bd79440a17801ea4f5157a22d58cec5a25344872975fda49f04b9d84dab6b9"
   else
-    url "https://github.com/into-docker/into-docker/releases/download/v1.1.5/into-1.1.5-macos-amd64.zip"
-    sha256 "20b520a2eb14c45c8a2cfce09bcf070f94c9a65215b70ffbd6a732331c0a1ccd"
+    url "https://github.com/into-docker/into-docker/releases/download/v1.1.6/into-1.1.6-macos-amd64.zip"
+    sha256 "87b2501158c1e3224b7f9707c6e99cc06fdc6163761644c840aa3781df5b4e75"
   end
 
   def install
